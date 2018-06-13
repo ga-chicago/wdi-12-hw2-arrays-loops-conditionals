789dc1be486b790a8042d2bb21ac602852b6d903
Jared Kranzler <kranzler312@gmail.com>
2018-06-13 17:54:20 -0500

    Commit 1-Easy Going answered

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// git add -A
+// git commit -m
+// git push origin master
+//completed the assignment and completed commits
+// make pull request, go to your fork/page make new pull request and submit
+console.log("javascript is harder than it looks and it looks hard")
+
+// 1. Write a for loop that will log the numbers 1 through 20.
+
+  // for (i = 1; i <= 20; i++){
+  //   console.log(i);
+  // } 
+
+// 2. 
+
+
