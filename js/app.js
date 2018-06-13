1310c74acd7d1567b3fbb4a6ebc055b32eca32b3
Jared Kranzler <kranzler312@gmail.com>
2018-06-13 18:00:20 -0500

    Commit 2 - Get Even ansered

--- js/app.js
+++ js/app.js
@@ -5,12 +5,26 @@
 // make pull request, go to your fork/page make new pull request and submit
 console.log("javascript is harder than it looks and it looks hard")
 
+// Easy Going
 // 1. Write a for loop that will log the numbers 1 through 20.
 
   // for (i = 1; i <= 20; i++){
   //   console.log(i);
   // } 
 
-// 2. 
+// Get Even
+// 1. Write a for loop that will log only the even numbers in 0 through 200.
+
+  for (i = 0; i <=200; i++){
+    if (i % 2 == 0){
+      console.log(i);
+    }
+  }
+
+
+
+
+
+
 
 
