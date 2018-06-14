0396328ac37f12ad8e52036d03b284c21b6b1401
Stephanie Obeytek <sobeytek@gmail.com>
2018-06-13 19:15:29 -0500

    Commit 3 - Excited Kittens answered -incomplete

--- js/app.js
+++ js/app.js
@@ -6,6 +6,22 @@ console.log("JavaScript is running!");
  // }
 
  //Write a for loop that will log only the even numbers in 0 through 200.
-for (let i = 0; i <= 200; i += 2) {
-  console.log(i);
-}
+// for (let i = 0; i <= 200; i += 2) {
+//   console.log(i);
+// }
+
+// 1. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+// const cat = ["...human...why you taking pictures of me?...", "...the catnip made me do it...","...why does the red dot always get away..."];
+//   for (let i = 0; i <= 20; i++){
+//     console.log("Love me, pet me! HSSSSSS!")
+//     if (i % 2 === 0){
+//     console.log(cat[Math.random()*100]);
+//     }
+//   }
+// 2. For every even number in your loop, log:
+// "...human...why you taking pictures of me?...", 
+//"...the catnip made me do it...", or 
+//"...why does the red dot always get away..." at random.
+
+
+
