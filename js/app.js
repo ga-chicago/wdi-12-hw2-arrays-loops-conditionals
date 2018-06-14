12133607651e03add6a77be604c3ab7111a5b8fe
David Kolia <Dkolia@icloud.com>
2018-06-13 21:42:05 -0500

    Get Even Answered

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 // Write a loop that goes from 1-20
-for (let i = 1; i < 21; i++){
-  console.log(i);
+// for (let i = 1; i < 21; i++){
+//   console.log(i);
+// }
+
+// Write a loop that will only log even numbers from 0-200
+for (let i = 0; i < 201; i++){
+  if(i % 2 === 0){
+    console.log((i));
+  };
 }
