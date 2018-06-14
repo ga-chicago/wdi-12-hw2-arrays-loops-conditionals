ec958ed21313a27f3ef77ed9de43c56c0a804164
Badal Moradia <badal@i360llc.com>
2018-06-13 19:40:00 -0500

    Commit 3 - Excited Kittens answered

--- js/app.js
+++ js/app.js
@@ -13,3 +13,18 @@ for (let i = 0; i <= 200; i += 2) {
   console.log(`The number is ${i}`);
 }
 
+//HW 3 Part 3: Excited Kitten
+for (let i = 0; i < 20; i++) {
+  console.log('Love me, pet me! HSSSSSS!');
+  catPick = Math.floor(Math.random() * Math.floor(3));
+  if (i % 2 === 0) {
+    if (catPick === 0) {
+      console.log("...human...why you taking pictures of me?...");
+    } else if ( catPick === 1) {
+      console.log("...the catnip made me do it...");
+    } else if (catPick === 2) {
+      console.log("...why does the red dot always get away...");
+    }
+  }
+}
+
