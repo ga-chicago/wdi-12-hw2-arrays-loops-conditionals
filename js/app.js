09f1f2df0504a87a97440f45d033f8596be643a9
Badal Moradia <badal@i360llc.com>
2018-06-13 19:30:40 -0500

    Commit 2 - Get Even answered

--- js/app.js
+++ js/app.js
@@ -5,3 +5,11 @@ console.log("Javascript is linked.")
 for (let i = 1; i <=20; i++) {
   console.log(`The number is ${i}`);
 }
+
+
+
+//HW 3 Part 2: Get Even
+for (let i = 0; i <= 200; i += 2) {
+  console.log(`The number is ${i}`);
+}
+
