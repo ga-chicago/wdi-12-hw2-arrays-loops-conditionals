af81196a4b3fa0dacfeec95de9602a46302fdfd6
Jared Kranzler <kranzler312@gmail.com>
2018-06-13 20:29:41 -0500

    commit 6 - Yell at the ninja Turtles answered

--- js/app.js
+++ js/app.js
@@ -48,7 +48,7 @@
 // 1. Write a javascript application that logs all numbers from 1 - 100.
 
       // for (let i = 1; i <= 100; i++) {
-      //   if ((i % 5 !== 0) && (i % 3 == 1)) {
+        // if ((i % 5 !== 0) && (i % 3 == 1)) {
       //    console.log(i + ". " + "Fizz");
       //    } else if ((i % 3 !== 0) && (i % 5 == 0)) {
       //     console.log(i + ". " + "Buzz");
@@ -57,24 +57,29 @@
       //   };
 
 // Getting to Know You
-const thom = ["Thom", 1000, "Christchurch"];
-const karolin = ["Karolin", 16, "New York"];
-const kristyn = ["Kristyn", 5, "Pittsburgh"];
-const matt = ["Matt H", 186, "Philadelphia"];
+// const thom = ["Thom", 1000, "Christchurch"];
+// const karolin = ["Karolin", 16, "New York"];
+// const kristyn = ["Kristyn", 5, "Pittsburgh"];
+// const matt = ["Matt H", 186, "Philadelphia"];
 
 
-thom[0] = "Gameboy";
-karolin[1] = "17";
-matt[2] = "Gotham City";
-kristyn[2] = "Brooklyn";
+// thom[0] = "Gameboy";
+// karolin[1] = "17";
+// matt[2] = "Gotham City";
+// kristyn[2] = "Brooklyn";
 
 
-console.log(thom);
-console.log(karolin);
-console.log(matt);
-console.log(kristyn);
+// console.log(thom);
+// console.log(karolin);
+// console.log(matt);
+// console.log(kristyn);
 
-
+// Yell at the Ninja Turtles
+const tmnt = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+ for (i = 0; i <= 3; i++){
+console.log(tmnt[i].toUpperCase());
+}
+// Return of the Closets
 
 
 
