d1c8b9b88d40183dd655d189d32efc7ef540a1ef
David Kolia <Dkolia@icloud.com>
2018-06-13 22:39:21 -0500

    Excited Kittens answered

--- js/app.js
+++ js/app.js
@@ -3,9 +3,23 @@
 //   console.log(i);
 // }
 
-// Write a loop that will only log even numbers from 0-200
-for (let i = 0; i < 201; i++){
-  if(i % 2 === 0){
-    console.log((i));
+// // Write a loop that will only log even numbers from 0-200
+// for (let i = 0; i < 201; i++){
+//   if(i % 2 === 0){
+//     console.log((i));
+//   };
+// }
+
+// Excited Kittens
+
+
+for (let i = 0; i <= 20; i++){
+  if(i < 20) {
+    console.log("Love me, pet me! HSSSSSS!")
+    if(i % 2 === 0){
+      let catCall = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+      let random = Math.floor(Math.random() * (4 - 1) * 1); //No idea how this works- just tested integers until I got it
+      console.log(catCall[random]);
+    }
   };
 }
