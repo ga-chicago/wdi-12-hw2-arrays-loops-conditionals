4e8111e43fc4278448d87cfbf8769010e8ae85cd
Steve <sbrennison@gmail.com>
2018-06-13 19:01:04 -0500

    Excited Kitten has defeated me

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
 
+// Section 1
 // 1.
-for(i = 1; i <= 20; i++){
-  console.log(i);
-}
+// for(const i = 1; i <= 20; i++){
+//   console.log(i);
+// }
 
 //2. 
-for(i = 0; i <= 200; i += 2){
-  console.log(i);
-}
+// for(const i = 0; i <= 200; i += 2){
+//   console.log(i);
+// }
+
+// Section 2
+// 1. 
+// for(let i = 0; i < 20; i++){
+//   console.log("Love me, pet me! HSSSSSS!");
+// }
+
+// 2.
+// for(let i = 0; i < 20; i++){
+//   console.log("...human...why you taking pictures of me?..", "...the catnip made me do it..", "why does the red dot always get away"); 
+//   if(i % 2 === 0){ 
+//   Math.random();
+// }
+// }
