9a91b432058f51403a667400466b67f51f2fb912
Jay Escala <Jonathan.J.Escala@gmail.com>
2018-06-14 14:30:04 -0500

    Commit 2 -Get Even Answered

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 console.log("The javascript is loaded.");
 
 // Easy Going
+// Q1.
 for(let i = 1; i <= 20; i++) {
+	console.log(i);
+}
+
+// Get Even
+// Q2.
+for(let i = 0; i <= 200; i+=2) {
 	console.log(i);
 }
 
