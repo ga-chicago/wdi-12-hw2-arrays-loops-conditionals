59ac007e6221b02667ecf302e0557df2d19080f3
Stephanie Obeytek <sobeytek@gmail.com>
2018-06-14 08:57:41 -0500

    Commit 6 - Yell at the Ninja Turtles answered(with error)

--- js/app.js
+++ js/app.js
@@ -39,16 +39,33 @@ console.log("JavaScript is running!");
   
 // }
 
-const thom = ["Thom", 1000, "Christchurch"]
-const karolin = ["Karolin", 16, "New York"]
-const kristyn = ["Kristyn", 5, "Pittsburgh"]
-const matt = ["Matt H", 186, "Philadelphia"]
+// const thom = ["Thom", 1000, "Christchurch"]
+// const karolin = ["Karolin", 16, "New York"]
+// const kristyn = ["Kristyn", 5, "Pittsburgh"]
+// const matt = ["Matt H", 186, "Philadelphia"]
 
-thom[0] = "Gameboy"
-console.log(thom);
-karolin[1] = 17
-console.log(karolin)
-matt[2] = "Gotham City"
-console.log(matt)
-kristyn[2] = "Brooklyn"
-console.log(kristyn)
+// thom[0] = "Gameboy"
+// console.log(thom);
+// karolin[1] = 17
+// console.log(karolin)
+// matt[2] = "Gotham City"
+// console.log(matt)
+// kristyn[2] = "Brooklyn"
+// console.log(kristyn)
+
+const TMNT = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+
+  for (let i = 0; i <= TMNT.length; i++){
+    console.log(TMNT[i].toUpperCase()) 
+  }
+
+ 
+
+
+
+
+
+
+
+
+
