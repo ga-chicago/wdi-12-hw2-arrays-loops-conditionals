0c0459d1109c17cf310b89be749da3bb21a81faa
Steve <sbrennison@gmail.com>
2018-06-14 08:31:41 -0500

    At least I was able to answer Getting to Know You

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 
 // Section 1
 // 1.
-// for(const i = 1; i <= 20; i++){
+// for(let i = 1; i <= 20; i++){
 //   console.log(i);
 // }
 
 //2. 
-// for(const i = 0; i <= 200; i += 2){
+// for(let i = 0; i <= 200; i += 2){
 //   console.log(i);
 // }
 
@@ -18,8 +18,46 @@
 
 // 2.
 // for(let i = 0; i < 20; i++){
-//   console.log("...human...why you taking pictures of me?..", "...the catnip made me do it..", "why does the red dot always get away"); 
 //   if(i % 2 === 0){ 
-//   Math.random();
+//     console.log("...human...why you taking pictures of me?..");
+//     console.log("...the catnip made me do it..");
+//     console.log("why does the red dot always get away"); 
+//     Math.random(i);
+//   }
 // }
-// }
+
+// Section 3
+// 1. 
+// for(let i = 1; i <= 100; i++){
+//     // console.log(i);
+// // 2.
+//     if(i % 3 !== 0){
+//       console.log("fizz");
+// // 3.
+//     } else if (i % 5 !== 0){
+//       console.log("buzz");
+// // 4.
+//     } else (i % 3 !== 0 && i % 5 === 0)
+//       console.log("fizzbuzz");
+//     }
+
+// Section 4
+// 1.
+// const thom = ["Thom", 1000, "Christchurch"]
+// const karolin = ["Karolin", 16, "New York"]
+// const kristyn = ["Kristyn", 5, "Pittsburgh"]
+// const matt = ["Matt H", 186, "Philadelphia"]
+
+//   thom[0] = ("Gameboy");
+//   console.log(thom);
+//   karolin[1] = 17;
+//   console.log(karolin);
+//   matt[2] = ("gotham City");
+//   console.log(matt);
+//   kristyn[2] = ("Brooklyn");
+//   console.log(kristyn);
+
+
+
+
+
