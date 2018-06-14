15d3c0ba4f8e90c0f3a9af7b4548e259d6fbd0d0
Badal Moradia <badal@i360llc.com>
2018-06-13 21:07:20 -0500

    Commit 9 - Project Euler Problem 1 answered

--- js/app.js
+++ js/app.js
@@ -119,7 +119,16 @@ for (let i = 0; i < thomsCloset.length; i++) {
   }
 }
 
+//HW 3 Part 9: Multiples of 3 and 5
+let sum = 0;
+for (let i = 0; i < 1000; i++) {
+  if ((i % 3 === 0) || (i % 5 === 0)) {
+    sum = sum + i;
+  }
+  
+}
 
+console.log(sum);
 
 
 
