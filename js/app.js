ef0ad7520c7b9d69a963552b323c993ec33f2aba
Jay Escala <Jonathan.J.Escala@gmail.com>
2018-06-17 20:58:29 -0500

    Commit 4 - Fizz Buzz answered.

--- js/app.js
+++ js/app.js
@@ -28,3 +28,17 @@ for(let i = 1; i <= 20; i++) {
 		console.log(phrases[randomNum]);
 	}
 }
+
+// Fizz Buzz
+// Q3.
+for(let i = 1; i <= 100; i++) {
+	if((i % 3 === 0) && (i % 5 === 0)) {
+		console.log("FizzBuzz");
+	} else if(i % 3 === 0) {
+		console.log("Fizz");
+	} else if(i % 5 === 0) {
+		console.log("Buzz");
+	} else {
+		console.log(i);
+	}
+}
