00d7b22b17a42121255dba31e1957ba4fa17851b
Jay Escala <Jonathan.J.Escala@gmail.com>
2018-06-17 20:50:32 -0500

    Commit 3 - Excited Kittens answered.

--- js/app.js
+++ js/app.js
@@ -7,8 +7,24 @@ for(let i = 1; i <= 20; i++) {
 }
 
 // Get Even
-// Q2.
+// Q1.
 for(let i = 0; i <= 200; i+=2) {
 	console.log(i);
 }
 
+// Excited Kitten
+// Q1.
+for(let i = 1; i <= 20; i++) {
+	console.log("Love me, pet me! HSSSSSS!");
+}
+
+// Q2.
+const phrases = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+
+for(let i = 1; i <= 20; i++) {
+	console.log("Love me, pet me! HSSSSSS!");
+	if(i % 2 == 0) {
+		let randomNum = (parseInt(Math.random()*3));
+		console.log(phrases[randomNum]);
+	}
+}
